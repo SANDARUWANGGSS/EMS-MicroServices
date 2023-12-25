92fbaebee73e0fbdd89932bf9d9ffeda2f064cbe
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2023-12-25 07:30:22 +0530

    added department entity

---
+++ department-service/src/main/java/com/MicroSrvicesL/departmentservice/entity/Department.java
@@ -0,0 +1,14 @@
+package com.MicroSrvicesL.departmentservice.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "department")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String departmentName;
+    private String departmentDescription;
+    private String departmentCode;
+}
